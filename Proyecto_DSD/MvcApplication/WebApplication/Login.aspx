c1aa009604ec60cfd99b1876f41f3feabfb3bec1
Jose Romucho <jose.romucho@gmail.com>
2013-01-25 16:46:48 -0500

    Fuentes del trabajo - Reniec

---
+++ Proyecto_DSD/MvcApplication/WebApplication/Login.aspx
@@ -0,0 +1,81 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Login.aspx.cs" Inherits="WebApplication.Login" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    
+    <link rel="Stylesheet" href="Layout/reset.css" />
+    <link rel="Stylesheet" href="Layout/typography.css" />
+    <link rel="Stylesheet" href="Layout/mem.css" />
+    <link rel="Stylesheet" href="Layout/jer.css" />
+    <link rel="Stylesheet" href="Layout/960.css" />
+    <link rel="Stylesheet" href="Layout/basic.css" />
+    <link rel="Stylesheet" href="Layout/jquery-ui.css" />
+    <link rel="Stylesheet" href="Layout/fileuploader.css" />
+    <link rel="Stylesheet" href="App_Themes/SkinGeneral/Default.css" />
+    <link rel="Stylesheet" href="Layout/slickmap.css" />
+
+    <script type="text/javascript" language="javascript">
+        function mensaje(msj) {
+            alert(msj);
+        }
+    </script>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div  align="center" >
+        <table border="0" cellpadding="0" cellspacing="0" width="100%"  >
+            <tr>
+                <td>
+                    <center><h1>Ingreso al Sistema</h1></center>
+                    <hr />
+                    <br />
+                    <br />
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <center>
+                    <table border="0" cellpadding="0" cellspacing="0">
+                        <tr>
+                            <td> 
+                                Número Documento :&nbsp;&nbsp;&nbsp; 
+                            </td>
+                            <td>
+                                <asp:TextBox ID="TextBox1" runat="server" />
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                Clave :&nbsp;&nbsp;&nbsp;
+                            </td>
+                            <td>
+                                <asp:TextBox ID="TextBox2" runat="server" />
+                            </td>
+                        </tr>
+                    </table> 
+                    </center>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <center>
+                    <br />                    
+                    <br />                    
+                    <asp:LinkButton ID="lbIngresar" runat="server" Text="Ingresar" onclick="lbIngresar_Click" />&nbsp;
+                    <br />
+                    <br />
+                    <br />
+                    Si no tiene Clave de Acceso o no la recuerda solicitela <a href="RegistroUsuario.aspx" >aquí</a>.
+                    </center>
+                    <br />
+                    <hr />
+                </td>
+            </tr>
+        </table>
+    </div>
+    </form>
+</body>
+</html>
---
+++ Proyecto_DSD/MvcApplication/WebApplication/RegistroUsuario.aspx.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using WebApplication.WebReference;
+
+namespace WebApplication
+{
+    public partial class RegistroUsuario : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            { 
+            }
+        }
+
+        protected void lbGrabar_Click(object sender, EventArgs e)
+        {
+            Grabar();
+            //Page.ClientScript.RegisterStartupScript(this.GetType(), "mensaje", "mensaje('El usuario fue creado satisfactiramente');", true);
+            Response.Redirect("Login.aspx", false);
+        }
+        
+        public void Grabar() 
+        {
+            try
+            {
+                SqlConnection cn = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionStringGarfield"].ConnectionString);
+                SqlCommand cmd = new SqlCommand();
+                cmd.CommandText = "usp_Registrar";
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.Add(new SqlParameter("@TipoUsuario", SqlDbType.Char, 1) { Value = ddlTipoPersona.SelectedValue });
+                cmd.Parameters.Add(new SqlParameter("@NombreCompleto", SqlDbType.VarChar, 50) { Value = TextBox1.Text });
+                cmd.Parameters.Add(new SqlParameter("@RazonSocial", SqlDbType.VarChar, 50) { Value = TextBox2.Text });
+                cmd.Parameters.Add(new SqlParameter("@TipoDocumento", SqlDbType.VarChar, 5) { Value = ddlTipoDocumento.SelectedValue });
+                cmd.Parameters.Add(new SqlParameter("@NumeroDocumento", SqlDbType.VarChar, 15) { Value = TextBox4.Text });
+                cmd.Parameters.Add(new SqlParameter("@TelefonoCelular", SqlDbType.VarChar, 9) { Value = TextBox5.Text });
+                cmd.Parameters.Add(new SqlParameter("@Email", SqlDbType.VarChar, 50) { Value = TextBox6.Text });
+                cmd.Parameters.Add(new SqlParameter("@Clave", SqlDbType.VarChar, 50) { Value = TextBox3.Text });
+                cmd.Connection = cn;
+                cn.Open();
+                cmd.ExecuteNonQuery();
+                cn.Close();
+                cn.Dispose();
+                cmd.Dispose();
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+
+        protected void lbVerificar_Click(object sender, EventArgs e)
+        {
+            Service1SoapClient obj = new Service1SoapClient();
+            PersonaDTO p = obj.buscarpersonaPorDNI(TextBox7.Text, 1);
+            if (p != null)
+            {
+                TextBox1.Text = p.txNomb;
+                TextBox6.Text = p.txMail;
+                TextBox4.Text = p.txNumDocu;
+            }
+            else
+            {
+                Page.ClientScript.RegisterStartupScript(this.GetType(), "mensaje", "mensaje('El usuario no se encuentra registrado');", true);
+            }
+        }
+
+
+    }
+}
